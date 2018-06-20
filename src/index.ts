309ad9ca49303944461015726fa6458fcc5d217d
Sam Redmond <sam.redmond@coveinsurance.co.nz>
2018-06-20 13:35:14 +1200

    Updating gitignore and adding upload route

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,11 @@ import * as cors from 'cors';
 import { connection } from './database';
 import { random } from './utils';
 import * as fs from 'fs';
+import * as path from 'path';
 
 dotenv.config();
+const defaultFolder = process.env.DEFAULT_FOLDER || 'DocBucket';
+const staticFolder = `${path.join(__dirname, '../')}/${defaultFolder}`;
 
 const app = express();
 const server = http.createServer(app);
@@ -15,10 +18,19 @@ const port = process.env.PORT || 8080;
 const corsOptions: cors.CorsOptions = {
   origin: true
 };
+
 app.use(cors(corsOptions));
 app.disable('x-powered-by');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/buckets', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const file = path.basename(req.url);
+  console.log(file);
+  return next();
+});
+app.use('/buckets', express.static(staticFolder));
 
 app.use(
   '/:apikey',
@@ -39,21 +51,26 @@ import * as userRoute from './routes/users';
 userRoute.Router.options('*', cors(corsOptions));
 app.use('/:apikey/users', userRoute.Router);
 
+//Upload Route
+import * as uploadRoute from './routes/upload';
+uploadRoute.Router.options('*', cors(corsOptions));
+app.use('/:apikey/upload', uploadRoute.Router);
+
 server.listen(port, async () => {
-  //Set up default folder
-  const defaultFolder = process.env.DEFAULT_FOLDER || 'DocBucket';
-  if (!fs.existsSync(defaultFolder)) fs.mkdirSync(defaultFolder);
+  if (!fs.existsSync(path.join(staticFolder, 'default')))
+    fs.mkdirSync(path.join(staticFolder, 'default'));
 
   const conn = await connection();
   const result = await conn
     .db()
     .collection('users')
     .findOne({ _id: 1 });
-  if (!result) {
+
+  if (!result)
     conn
       .db()
       .collection('users')
       .save({ _id: 1, username: 'root', password: 'D0cBucket!', apikey: random });
-  }
+
   console.log(`DocBucker Server Started On Port ${port}`);
 });
---
+++ src/routes/upload.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+const router = express.Router();
+
+
+router.post('/:path', async (req: express.Request, res: express.Response) => {
+
+});
+
+
+export const Router = router;
